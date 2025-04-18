a921035a441fb824719f40402a02f65fe7eb60dc
Ed Fazylov <efazylov@inno.tech>
2025-04-19 02:06:40 +0300

    Изменена основная логика. Добавлен класс Коробка

---
+++ src/main/java/com/stepup/proj/Box.java
@@ -0,0 +1,22 @@
+package com.stepup.proj;
+
+public class Box {
+    public int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Box() {
+
+
+    }
+
+    public Box(int size) {
+        this.size = size;
+    }
+}
--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,11 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("1111111");
+        System.out.println("110011");
+    }
+
+    public static void doSomethind{
+        System.out.println(1);
+
     }
 }
